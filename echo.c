fd9f0ffadcd225cdce70dbaf3c319105751d683c
art <art>
2001-05-23 22:01:44 +0000

    pretty

--- echo.c
+++ echo.c
@@ -357,9 +357,9 @@ complt(flags, c, buf, cpos)
 	if ((flags & EFFUNC) != 0) {
 		buf[cpos] = '\0';
 		wholelist = lh = complete_function_list(buf, c);
-	} else if ((flags & EFBUF) != 0)
+	} else if ((flags & EFBUF) != 0) {
 		lh = &(bheadp->b_list);
-	else if ((flags & EFFILE) != 0) {
+	} else if ((flags & EFFILE) != 0) {
 		buf[cpos] = '\0';
 		wholelist = lh = make_file_list(buf);
 	} else
