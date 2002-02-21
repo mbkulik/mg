36f60447f65e2dc6b4aafc93b3f969221fa9430e
dhartmei <dhartmei>
2002-02-21 00:04:10 +0000

    It seems you need to have hacked mg at some point to be considered a true old fart, so here's my contribution ;)
    Don't use the same va_list twice without re-va_start()ing it, doesn't
    work on macppc.

--- buffer.c
+++ buffer.c
@@ -343,15 +343,18 @@ addlinef(BUFFER *bp, char *fmt, ...)
 	char   dummy[1];
 
 	va_start(ap, fmt);
-	ntext = vsnprintf(dummy, 1, fmt, ap) + 1;
+	ntext = vsnprintf(dummy, 1, fmt, ap);
 	if (ntext == -1) {
 		va_end(ap);
 		return FALSE;
 	}
+	ntext++;
 	if ((lp = lalloc(ntext)) == NULL) {
 		va_end(ap);
 		return FALSE;
 	}
+	va_end(ap);
+	va_start(ap, fmt);
 	vsnprintf(lp->l_text, ntext, fmt, ap);
 	lp->l_used--;
 	va_end(ap);
