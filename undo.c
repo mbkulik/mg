b53e0e215293a4bc91d3b5385706bb994afcc7d6
vincent <vincent>
2002-02-21 03:24:14 +0000

    Typo and...
     Duh, newlines aren't even stored in the lines' content, check for
     a 0 length line to detect deleting newlines.

--- undo.c
+++ undo.c
@@ -70,7 +70,7 @@ find_offset(LINE *lp, int off)
 
 	for (p = curwp->w_linep; p != lp; p = lforw(p)) {
 		if (count != 0) {
-			if (p == curbp->b_linep) {
+			if (p == curwp->w_linep) {
 				ewprintf("Error: Undo stuff called with a"
 				    "nonexistent line\n");
 				return FALSE;
@@ -265,9 +265,10 @@ undo_add_delete(LINE *lp, int offset, int size)
 	reg.r_size = size;
 
 	pos = find_offset(lp, offset);
-	
-	if (size == 1 && lgetc(lp, offset) == '\n')
+
+	if (size == 1 && llength(lp) == 0) {
 		skip = 1;
+	}
 
 	/*
 	 * Again, try to reuse last undo record, if we can
