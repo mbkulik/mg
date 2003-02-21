1288bb8a3d151984377ba10fcb404f4a45c8da6d
mcbride <mcbride>
2003-02-21 14:00:36 +0000

    A little scatological humour goes a long way.
    "this is fine" henning@

--- theo.c
+++ theo.c
@@ -102,7 +102,8 @@ static const char *talk[] = {
 	"Search for fuck.  Anytime you see that word, you have a paragraph to write.",
 	"what I'm doing [...] is hell. it's kind of fun.",
 	"Yes, but the ports people are into S&M.",
-	"Buttons are for idiots."
+	"Buttons are for idiots.",
+	"We are not hackers. We are turd polishing craftsmen."
 };
 
 static const int ntalk = sizeof(talk)/sizeof(talk[0]);
