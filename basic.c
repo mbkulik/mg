70d0be8a8aee8549967c4ab930901038b350df9d
deraadt <deraadt>
2003-09-19 21:20:47 +0000

    do not += snprintf; andrushock@korovino.net

--- basic.c
+++ basic.c
@@ -239,8 +239,9 @@ getgoal(LINE *dlp)
 			col++;
 		else {
 			char tmp[5];
-			col += snprintf(tmp, sizeof tmp, "\\%o",
-			    c);
+
+			snprintf(tmp, sizeof tmp, "\\%o", c);
+			col += strlen(tmp);
 		}
 		if (col > curgoal)
 			break;
