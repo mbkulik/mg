9a8ce84c1e311be39301698b6702763b9f431b21
jcs <jcs>
2005-05-01 03:43:58 +0000

    another

--- theo.c
+++ theo.c
@@ -171,7 +171,8 @@ static const char *talk[] = {
 	"openbsd development is slow because lots of developers have shrunken balls",
 	"penguins are not much more than chickens that swim.",
 	"i am a packet sniffing fool, let me wipe my face with my own poo",
-	"they are manual pages, not tea time chit-chats"
+	"they are manual pages, not tea time chit-chats",
+	"Whiners.  They scale really well."
 };
 
 static const int ntalk = sizeof(talk)/sizeof(talk[0]);
