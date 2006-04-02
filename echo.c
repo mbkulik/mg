431ef24c906c2695432d0fbf605391be857f9113
kjell <kjell>
2006-04-02 17:18:58 +0000

    fix ^W (delete to start of word) in minibuffer; i.e. actually kill text, don't just move the pointer.

--- echo.c
+++ echo.c
@@ -420,6 +420,7 @@ veread(const char *fp, char *buf, size_t nbuf, int flag, va_list ap)
 					ttputc('\b');
 					--ttcol;
 				}
+				epos--;
 			}
 			while ((cpos > 0) && ISWORD(buf[cpos - 1])) {
 				ttputc('\b');
@@ -432,6 +433,7 @@ veread(const char *fp, char *buf, size_t nbuf, int flag, va_list ap)
 					ttputc('\b');
 					--ttcol;
 				}
+				epos--;
 			}
 			ttflush();
 			break;
