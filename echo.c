f8c6eb57beb5553cc52e65875b62aa222b2e07e6
florian <florian>
2012-11-03 16:26:32 +0000

    kill dead assignment, found by llvm
    ok haesbaert, lum, benno

--- echo.c
+++ echo.c
@@ -268,7 +268,6 @@ veread(const char *fp, char *buf, size_t nbuf, int flag, va_list ap)
 		case CCHR('D'):
 			if (cpos != epos) {
 				tteeol();
-				y = buf[cpos];
 				epos--;
 				rr = ttrow;
 				cc = ttcol;
